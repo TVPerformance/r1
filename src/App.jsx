6815183ba032110df7850238c801946288111269
TVPerformance <tomas.visockas2@gmail.com>
2022-11-28 15:01:53 +0200

    states

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import './App.css';
-import Valio from './Components/Valio.jsx';
-import Rand from './Functions/Rand.js'
-function App() {
+import './App.scss';
+import {useState} from 'react';
+
+function App() { 
+  
+ const [color, setColor] = useState(true); 
+ const [count, setCount] = useState(1);
+
+ const now777 = (a) => {
+  console.log ('NOW is ' + a);
+}
+ 
+ const change = () => {
+  setColor(!color); // Neteisinga!
+ }
+
+ const add = () => {
+  setCount(count => count +1) // Redagavimui 
+  setCount(count => count +1)
+  setCount(count => count +1)
+ }
+
+ const set42 = () => {
+  const meaningOfLife = 42; // Priskyrimui
+  setCount(meaningOfLife);
+ }
+  
   return (
+
     <div className="App">
-      <header className="App-header" style={{color: Rand(0, 1)? 'red' : 'blue'}}>
-      <Valio></Valio>
-      </header>
+      <div className="App-header">
+      <h1 style={{color: color ? 'crimson' : 'skyblue'}}>Bebras {count}</h1>
+      <button onClick={change}>Do color</button>
+      <button onClick={add}>Add</button>
+      <button onClick={set42}>42</button>
+      <button onClick={() => now777('9:00')}>Press on 9:00 PM</button>
+      <button onClick={() => now777('10:00')}>Press on 10:00 PM</button>
+      <button onClick={now777}>Press on 11:00 PM</button>
+      <button onClick={() => console.log('NOW!!!!')}>Press on 7:00 PM</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
